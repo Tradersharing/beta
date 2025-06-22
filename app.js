e67a0f52e1c6d488beca938cdfb090d4f4ebf8a8
Tradersharing <ardiyulistyanto3@gmail.com>
2025-06-22 13:21:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ async function buatAnalisaSekarang() {
 
   analysisPopup.innerHTML = `
 <div class="analysis-main">
-  <div class="corner-label">📊Analisa pair ${pairName} ${today}</div>
+  <div class="corner-label">📊Analisa pair ${pair.name} </div>
   <pre id="typeWriter"></pre>
   <div class="footer">
     <button onclick="closeAnalysis()">Tutup</button>
