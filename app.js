72d29022bf79e75202c87f2ddf848d8353ab1013
Tradersharing <ardiyulistyanto3@gmail.com>
2025-06-24 13:47:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -421,3 +421,28 @@ async function loadSignals() {
 
 loadSignals();
 setInterval(loadSignals, 60000);
+function hitungSinyalDariHarga(pair, h1, h4) {
+  let signal, strength;
+
+  if (h4.close > h4.open && h1.close > h1.open) {
+    signal = "BUY";
+    strength = getRandom(70, 85);
+  } else if (h4.close < h4.open && h1.close < h1.open) {
+    signal = "SELL";
+    strength = getRandom(70, 85);
+  } else {
+    signal = "WAIT";
+    strength = getRandom(31, 69);
+  }
+
+  return {
+    pair: pair,
+    signal: signal,
+    strength: strength
+  };
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
