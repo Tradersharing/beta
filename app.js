6a477cbb226352cb5e02a1f168df62935c43c4f7
Tradersharing <ardiyulistyanto3@gmail.com>
2025-06-25 05:48:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -470,7 +470,7 @@ async function tampilkanInsightBerita(pair) {
   }
 }
 
-const signalsUrlPrimary = "https://script.google.com/macros/s/AKfycbyv_7XI1NwCNj5keHuzqYt-7dKg3Vx-3_HiYxieeGQEZ1jfAmhAhbBuqp7DNRli-20sbA/exec"; // ✅ Google Script utama
+const signalsUrlPrimary = "https://script.google.com/macros/s/AKfycbz0nOF6LQWbQd4Zy9WagBiq433l0G0nn2la4j9QC73Vfgt3_Fz_hRuhH3fr0NiPt46rOA/exec"; // ✅ Google Script utama
 const signalsUrlBackup = "https://script.google.com/macros/s/AKfycby4rTfuD0tr1XuJU4R-MUacv85WRu3_ucD7QOiC11ogkupkEhXRjSF7ll0GrTgoJQqP/exec"; // 🔁 MyFXBook-style backup
 
 async function loadSignals(url = signalsUrlPrimary) {
